82a7331dea75b6ebc1f8f12a977e04bd371bb874
allen <272970666@qq.com>
2022-09-09 21:21:00 +0800

    feat: hidden csdn login dialog

--- csdn.js
+++ csdn.js
@@ -6,13 +6,17 @@
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  "use strict";
+(function() {
+  'use strict';
   // 免登录复制
-  document.designMode = "on";
+  const heads = document.querySelector('head');
+  const style = document.createElement('style');
+  style.setAttribute('type', 'text/css');
+  style.innerHTML = `pre,code{user-select:auto !important}.passport-login-container{display: none !important;}`;
+  heads.append(style);
   // 移除复制后缀
-  document.querySelector("body").addEventListener("copy", (event) => {
-    const selection = document.getSelection();
-    event.clipboardData.setData("text/plain", selection.toString());
+  document.querySelector('body').addEventListener('copy', (event) => {
+      const selection = document.getSelection();
+      event.clipboardData.setData('text/plain', selection.toString());
   });
 })();
